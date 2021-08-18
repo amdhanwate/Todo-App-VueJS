72c530e5c962aa6b109690c899dd094edcd129f5
= <=>
2021-08-18 14:46:56 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## TODO APP | VeuJS
+
+#### Technologies
+**Basic**
+- HTML
+- CSS
+- JavaScript
+
+**CSS Frameworks**
+- Milligram.io
+- Normalize CSS
+
+**JS Frameworks**
+- VueJS
+
+#### Screenshot
+
+![Todo App with zero Todos](Screenshots/none.PNG)
+<hr />
+
+![Todo App with some Todos](Screenshots/added.PNG)
+<hr />
---
+++ main.js
@@ -0,0 +1,60 @@
+new Vue({
+    el: "#app",
+
+    data: {
+        isEditing: false,
+        selectedIndex: null,
+        todo: "",
+        todos: JSON.parse(localStorage.getItem("todos")),
+        todosLength: JSON.parse(localStorage.getItem("todos")).length 
+    },
+
+    methods: {
+        storeTodo() {
+            if ((this.todo.trim()).length !== 0){
+                if (this.todos) {
+                    this.todos.push(this.todo)
+                    this.todo = ""
+                } else {
+                    this.todos = []
+                    this.todos.push(this.todo)
+                    this.todo = ""
+                }
+            }
+
+            localStorage.setItem("todos", JSON.stringify(this.todos))
+            this.todosLength = (JSON.parse(localStorage.getItem("todos"))).length
+
+            document.getElementsByTagName("input")[0].focus()
+            
+        },
+
+        editTodo(todo, index) {
+            // console.log(index, this.todo)
+            this.isEditing = true
+            this.selectedIndex = index
+            this.todo = todo
+        },
+
+        updateTodo() {
+
+            this.todos.splice(this.selectedIndex, 1, this.todo)
+            this.todo = ''
+            this.isEditing = false
+
+            localStorage.setItem("todos", JSON.stringify(this.todos))
+        },
+
+        cancelUpdate() {
+            this.todo = ""
+            this.isEditing = false
+        },
+
+        deleteTodo(index) {
+            this.selectedIndex = index
+            this.todos.splice(this.selectedIndex, 1)
+            localStorage.setItem("todos", JSON.stringify(this.todos))
+            this.todosLength = (JSON.parse(localStorage.getItem("todos"))).length
+        }
+    }
+})
